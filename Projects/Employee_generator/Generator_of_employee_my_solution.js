c7c78ab835a8261d6294d5b30e9e4b7aa27467f2
richnx <xenia.richnakova.ext@msg.group>
2022-11-29 23:33:24 +0100

    Validation added

--- Projects/Employee_generator/Generator_of_employee_my_solution.js
+++ Projects/Employee_generator/Generator_of_employee_my_solution.js
@@ -1,3 +1,7 @@
+const Crypto = require("crypto");
+const fs = require("fs");
+
+
 const dtoOut = [{
     gender: "male",
     birthdate: "1993-08-07T00:00:00.000Z",
@@ -9,10 +13,38 @@ const dtoOut = [{
   const input_data = {
     count: 20,  // amount of employees
     age: {      // age range in between the employee should be generated
-      min: 19, 
-      max: 35
+      min: 20, 
+      max: 40
     }
+
   }
+
+
+  // Validation
+  function main(input_data){
+
+    if( !( typeof(input_data.count) === 'number') || input_data.count <=0 ) {
+      throw 'Amount of employees must be a number higher than 0'; 
+    } 
+  
+    if( !( typeof(input_data.age.min) === 'number') || !( typeof(input_data.age.max) === 'number') || input_data.age.min <=0 || input_data.age.max <=0 ) {
+      throw 'Age must be a number higher than 0';
+    }
+
+    if( input_data.age.min < 18 || input_data.age.min > 75 ) {
+      throw 'Minimal range of ages must be between 18 and 75'; 
+      }
+
+    if( input_data.age.max < 18 || input_data.age.max > 75 ) {
+      throw 'Maximal range of ages must be between 18 and 75'; 
+      }
+
+      else {
+        return generateEmployees(input_data); 
+        }
+    }
+
+
   
   
   const randWorkloads = [10, 20, 30, 40];
@@ -22,6 +54,7 @@ const dtoOut = [{
   const female_name2 = ["Žáková","Vávrová","Vaňková","Tichá","Veselá","Lišková","Kovářová","Bártová","Zemanová","Valentová","Němečková","Moravcová","Bednářová","Macháčková","Urbanová","Žáková","Vaňková","Vávrová"];
 
     // now we store into to the variable all the keys of given array, number of every key is an index of every element of the array 
+      // if there would be not these 3 dots, we would store just a first index of every array = all the objects wuld be same 
   let maleNameIndexes = [...Array(male_name1.length).keys()];
   let maleSurNameIndexes = [...Array(male_name2.length).keys()];
   let femaleNameIndexes = [...Array(female_name1.length).keys()];
@@ -38,12 +71,24 @@ function getRandomItem(arr) {
 }
 
 
-  // this will randomly choose an value in between the range we already defined ininput_data
+  // this will randomly choose an value in between the range we already defined in input_data
   function getRandomArbitrary(min, max) {
     return parseInt(Math.floor(Math.random() * (max - min) + min));
   }
+
+  function getRandomBirthDate({ min, max }) {
+    const today = new Date();
+      // randomAge generate random number between min and max, e. g. 22.36
+    const randomAge = Math.floor((Math.random() * (max - min) + min) * 100) / 100;
+      // this makes from year (age) 22.36 a value in milisecond
+    const randomMilis = randomAge * 1000 * 60 * 60 * 24 * 365;
+    return new Date(today.setTime(today.getTime() - randomMilis));
+  }
   
-  function main(input_data) {
+  function generateEmployees(input_data) {
+
+    
+
     // how many employees work in the company
     let dtoIn_counter = input_data.count
 
@@ -98,11 +143,12 @@ function getRandomItem(arr) {
 
       // now we have defined structure of the object newEmployee
       const newEmployee = {
+        id: Crypto.randomBytes(4).toString("hex"),
         name,
         surname,
         gender,
         workload,
-        age
+        dateOfBirth: getRandomBirthDate(input_data.age),
       }
       return [...acc, newEmployee]
     }, [])
@@ -111,10 +157,13 @@ function getRandomItem(arr) {
 
   }
   
-  console.log(main(input_data))
+ const result = main(input_data);
+ fs.writeFileSync("./result.json", JSON.stringify(result, null, 2));
 
- /* const today = new Date("25 November 2022 14:48 UTC");
-  console.log(today.toISOString());
-*/
+/* const employee = new main(input_data);
 
-
+ main.prototype.toString = function employeeToString() {
+   return `${main.name}`;
+ };
+ 
+ console.log(employee.toString()); */
